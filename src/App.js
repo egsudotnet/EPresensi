14d93fc7ba65b6a5fbeeb4d888b94f662ef45e75
egsu <egsudotnet@gmail.com>
2021-12-31 10:46:16 +0700

    Inisisasi

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {Home, NewsDetail} from './pages';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      {/* <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="News Detail" component={NewsDetail} />
+      </Stack.Navigator> */}
+    </NavigationContainer>
+  );
+};
+
+export default App;
